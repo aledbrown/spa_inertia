0f90622f57f379db0aa1301926d7b8fa3b956c94
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-12-11 15:35:29 +0000

    BulkEdit beginnings

---
+++ resources/js/Pages/Product/BulkEdit.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Modal from "@/Components/Modal.vue";
+
+const props = defineProps({
+    show: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <Modal :show="show" max-width="xl">
+        <h3>Bulk Edit</h3>
+    </Modal>
+</template>
